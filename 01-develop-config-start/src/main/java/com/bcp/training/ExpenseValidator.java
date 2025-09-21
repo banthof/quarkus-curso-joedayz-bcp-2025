e075c779ff59fcea8d16b926d8a4b7468192908e
Anthony Aldair Flores Berrocal <anthony200699@hotmail.com>
2025-09-21 14:25:29 -0500

    clase 1 repasado y terminado

--- 01-develop-config-start/src/main/java/com/bcp/training/ExpenseValidator.java
+++ 01-develop-config-start/src/main/java/com/bcp/training/ExpenseValidator.java
@@ -2,22 +2,36 @@ package com.bcp.training;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class ExpenseValidator {
 
-    @Inject
-    ExpenseConfiguration config;
+//    private static final boolean DEBUG_EMABLED = true;
+    @ConfigProperty(name = "debug-enabled", defaultValue = "false")
+    boolean debugEnabled;
+
+//    private static final int RANGE_HIGH = 1000;
+    @ConfigProperty(name = "range-high")
+    int targetRangeHigh;
+
+//    private static final int RANGE_LOW = 250;
+    @ConfigProperty(name = "range-low")
+    int targetRangeLow;
 
     public void debugRanges() {
-        config.debugMessage().ifPresent(System.out::println);
+//        System.out.println("Range - High: " + RANGE_HIGH);
+        System.out.println("Range - High: " + targetRangeHigh);
+//        System.out.println("Range - Low: " + RANGE_LOW);
+        System.out.println("Range - Low: " + targetRangeLow);
     }
 
     public boolean isValidAmount(int amount) {
-        if (config.debugEnabled()) {
+        if (debugEnabled) {
             debugRanges();
         }
 
-        return amount >= config.rangeLow() && amount <= config.rangeHigh();
+//        return amount >= RANGE_LOW && amount <= RANGE_HIGH;
+        return amount >= targetRangeLow && amount <= targetRangeHigh;
     }
 }
