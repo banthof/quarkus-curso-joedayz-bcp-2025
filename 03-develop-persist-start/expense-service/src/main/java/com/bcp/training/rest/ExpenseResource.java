7e82095800a3a713d7a91c6f0144c21cdb0bb389
Anthony Aldair Flores Berrocal <anthony200699@hotmail.com>
2025-09-24 11:44:23 -0500

    add panache and openapi annotations - class 3

--- 03-develop-persist-start/expense-service/src/main/java/com/bcp/training/rest/ExpenseResource.java
+++ 03-develop-persist-start/expense-service/src/main/java/com/bcp/training/rest/ExpenseResource.java
@@ -23,39 +23,59 @@ import com.bcp.training.model.Expense;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Page;
 import io.quarkus.panache.common.Sort;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/expenses")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Tag(name = "Expense Management", description = "Operations for managing expenses")
 public class ExpenseResource {
 
     @GET
-    // TODO 1: Implement with a call to "listAll()" of Expense entity.
     // TODO 2: Add pagination and sort by "amount" and "associateId".
+    @Operation(
+            summary = "Get all expenses",
+            description = "Retrieve a list of all expenses in the system"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Successful retrieval of expenses list"
+    )
     public List<Expense> list() {
-        return null;
+        return Expense.listAll();
     }
 
     @POST
-    // TODO: Make the method transactional
+    @Transactional
     public Expense create(final Expense expense) {
-        Expense newExpense = Expense.of(expense.name, expense.paymentMethod,
-                expense.amount.toString(), expense.associateId);
-        // TODO: Use the "persist()" method of the entity.
+
+        Expense newExpense = Expense.of(expense.name, expense.paymentMethod, expense.amount.toString(), expense.associateId);
+
+        newExpense.persist();
 
         return newExpense;
     }
 
     @DELETE
     @Path("{uuid}")
-    // TODO: Make the method transactional
+    @Transactional
     public void delete(@PathParam("uuid") final UUID uuid) {
-        // TODO: Use the "delete()" method of the entity and list the expenses
+        long numExpensesDeleted = Expense.delete("uuid", uuid);
+        if (numExpensesDeleted == 0) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+
     }
 
     @PUT
-    // TODO: Make the method transactional
+    @Transactional
     public void update(final Expense expense) {
-        // TODO: Use the "update()" method of the entity.
+        try {
+            Expense.update(expense);
+        } catch (RuntimeException e) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
     }
 }
